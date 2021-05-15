d774dbbfe56cc9d91c6bf817b1aafcbf4fd2931b
Neel <letsmovelove@gmail.com>
2021-05-15 23:01:00 +0530

    Including tests

--- product-management/src/main/java/com/product/management/ProductManagementApplication.java
+++ product-management/src/main/java/com/product/management/ProductManagementApplication.java
@@ -2,9 +2,11 @@ package com.product.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan("com.sharma.nks.products")
 public class ProductManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductManagementApplication.class, args);
---
+++ product-management/src/test/java/com/product/management/ProductManagementApplicationTest.java
@@ -0,0 +1,12 @@
+package com.product.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductManagementApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
