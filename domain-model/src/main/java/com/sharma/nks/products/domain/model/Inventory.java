76a23a7ca438264d22636310dcec54f85c4768ab
Neel <letsmovelove@gmail.com>
2021-08-07 15:36:42 +0530

    sizing inventory

--- domain-model/src/main/java/com/sharma/nks/products/domain/model/Inventory.java
+++ domain-model/src/main/java/com/sharma/nks/products/domain/model/Inventory.java
@@ -9,13 +9,16 @@ import java.util.Map;
 @Entity
 @Data
 public class Inventory {
+    public static int MAX = 10;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     @ElementCollection(fetch = FetchType.LAZY)
-    @MapKeyColumn(name="productId")
-    @Column(name="units")
-    private Map<Long, Integer> productIdToUnits= new HashMap<>();
+    @MapKeyColumn(name = "productId")
+    @Column(name = "units")
+    private Map<Long, Integer> productIdToUnits = new HashMap<>(MAX);
     private int capacity;
     private Type type;
 
